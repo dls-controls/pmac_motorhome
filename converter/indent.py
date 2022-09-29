02dc3af2150e6459c50a12b092ae90c1864da1e8
UrszulaNeuman <urszula.neuman@gmail.com>
2022-09-29 11:18:30 +0100

    refactor make_code function

---
+++ converter/indent.py
@@ -0,0 +1,13 @@
+class Indenter:
+    def __init__(self):
+        self.level = -1
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.level -= 1
+
+    def format_text(self, text):
+        return("    " * self.level + text +"\n")
